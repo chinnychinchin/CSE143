1650eeb0f9b4138ef78b4fe2567bf800119fe838
Lim Chin Yan <d0106549@ncsdevnet.com>
2023-01-27 18:00:54 +0800

    completed hw1 stage 2

--- hw1/LetterInventory/src/LetterInventory.java
+++ hw1/LetterInventory/src/LetterInventory.java
@@ -1,25 +1,85 @@
 public class LetterInventory {
 
-    private int size; // total count of all the letters in the inventory
-    private int[] charCount; // an integer array representing the number of counts of each alphabet.
-    // e.g. the int stored at index 0 of this array is the number of 'a's there are in upper and lower case
-    private static final int NUMBER_OF_LETTERS = 26;
+  private int size; // total count of all the letters in the inventory
+  private int[] charCount; // an integer array representing the number of counts of each alphabet.
+  // e.g. the int stored at index 0 of this array is the number of 'a's there are
+  // in upper and lower case
+  private static final int NUMBER_OF_LETTERS = 26;
 
-    public LetterInventory (String data) {
+  public LetterInventory(String data) {
 
-        charCount = new int[NUMBER_OF_LETTERS];
+    charCount = new int[NUMBER_OF_LETTERS];
 
-        // Loop through the string
-        for (int i = 0; i < data.length(); i++) {
+    // Loop through the string
+    for (int i = 0; i < data.length(); i++) {
 
-            char ch = data.charAt(i);
-            if (Character.isLetter(ch)) {
-                int displacement = Character.toLowerCase(ch) - 'a';
-                charCount[displacement]++;
-                size++;
+      char ch = data.charAt(i);
+      if (Character.isLetter(ch)) {
+        int displacement = checkDisplacement(ch);
+        charCount[displacement]++;
+        size++;
 
-            }
-        }
+      }
+    }
+
+  }
+
+  public int size() {
+    return size;
+  }
+
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  private int checkDisplacement(char ch) {
+
+    return Character.toLowerCase(ch) - 'a';
+  }
+
+  // pre : char passed must be alphabetic (throws IllegalArgumentException if not)
+  // post: returns a count of how many of this letter are in the inventory.
+  public int get(char ch) {
+
+    if (!Character.isLetter(ch)) {
+      throw new IllegalArgumentException();
+    }
+
+    int displacement = checkDisplacement(ch);
+    return charCount[displacement];
+
+  }
+
+  public String toString() {
+
+    if (size == 0) {
+      return "[]";
+    }
+
+    String result = "[";
+    for (int i = 0; i < NUMBER_OF_LETTERS; i++) {
+      char ch = (char) ('a' + i);
+      int numOfLetters = charCount[i];
+      String toAdd = Character.toString(ch).repeat(numOfLetters);
+      result += toAdd;
 
     }
+    result += "]";
+    return result;
+
+  }
+  
+  // pre : If a nonalphabetic character is passed or if value is negative, throw an IllegalArgumentException.
+  // post: Sets the count for the given letter to the given value.
+  public void set(char letter, int value) {
+   if (!Character.isLetter(letter) || value < 0) {
+      throw new IllegalArgumentException();
+   }
+   
+   int displacement = checkDisplacement(letter);
+   int currentCount = charCount[displacement];
+   int difference = value - currentCount;
+   charCount[displacement] = value;
+   size += difference;
+  }
 }
---
+++ hw1/LetterInventory/src/LetterInventoryClient.java
@@ -0,0 +1,19 @@
+public class LetterInventoryClient {
+
+     public static void main(String[] args) {
+
+          LetterInventory li = new LetterInventory("+++bb123");
+          System.out.println("size = " + li.size());
+          System.out.println("isEmpty = " + li.isEmpty());
+          System.out.println(li.get('a'));
+          
+          li.set('a',2);
+          System.out.println("li = " + li);
+          li.set('c',3);
+          System.out.println("li = " + li);
+          li.set('b',0);
+          System.out.println("li = " + li);
+          System.out.println("size = " + li.size());
+     }
+
+}
