d5b9e011f06e76630aa8e3b8ef99e453e2f1ea3f
Lim Chin Yan <d0106549@ncsdevnet.com>
2023-02-03 12:00:53 +0800

    formatted style for hw1 code. Completed hw1

--- hw1/LetterInventory/src/LetterInventory.java
+++ hw1/LetterInventory/src/LetterInventory.java
@@ -68,54 +68,57 @@ public class LetterInventory {
     return result;
 
   }
-  
-  // pre : If a nonalphabetic character is passed or if value is negative, throw an IllegalArgumentException.
+
+  // pre : If a nonalphabetic character is passed or if value is negative, throw
+  // an IllegalArgumentException.
   // post: Sets the count for the given letter to the given value.
   public void set(char letter, int value) {
-   if (!Character.isLetter(letter) || value < 0) {
+    if (!Character.isLetter(letter) || value < 0) {
       throw new IllegalArgumentException();
-   }
-   
-   int displacement = checkDisplacement(letter);
-   int currentCount = charCount[displacement];
-   int difference = value - currentCount;
-   charCount[displacement] = value;
-   size += difference;
+    }
+
+    int displacement = checkDisplacement(letter);
+    int currentCount = charCount[displacement];
+    int difference = value - currentCount;
+    charCount[displacement] = value;
+    size += difference;
   }
-  
+
   public LetterInventory add(LetterInventory other) {
-   // Create a new LetterInventory object first
-   LetterInventory newLetterInventory = new LetterInventory("");
-   int newSize = this.size + other.size;
-   // set the newLetterInventory size attribute 
-   newLetterInventory.size = newSize;
-   int[] newCharCount = new int[NUMBER_OF_LETTERS];
-   for (int i = 0; i < NUMBER_OF_LETTERS; i++) {
+    // Create a new LetterInventory object first
+    LetterInventory newLetterInventory = new LetterInventory("");
+    int newSize = this.size + other.size;
+    // set the newLetterInventory size attribute
+    newLetterInventory.size = newSize;
+    int[] newCharCount = new int[NUMBER_OF_LETTERS];
+    for (int i = 0; i < NUMBER_OF_LETTERS; i++) {
       newCharCount[i] = charCount[i] + other.charCount[i];
-   }
-   // set the newLetterInventory charCount attribute
-   newLetterInventory.charCount = newCharCount;
-   
-   return newLetterInventory;
+    }
+    // set the newLetterInventory charCount attribute
+    newLetterInventory.charCount = newCharCount;
+
+    return newLetterInventory;
   }
-  
+
   public LetterInventory subtract(LetterInventory other) {
-  
-   int newSize = this.size - other.size;
-   if (newSize < 0) return null;
-   int[] newCharCount = new int[NUMBER_OF_LETTERS];
-   for (int i = 0; i < NUMBER_OF_LETTERS; i++) {
+
+    int newSize = this.size - other.size;
+    if (newSize < 0)
+      return null;
+    int[] newCharCount = new int[NUMBER_OF_LETTERS];
+    for (int i = 0; i < NUMBER_OF_LETTERS; i++) {
       newCharCount[i] = charCount[i] - other.charCount[i];
-      if (newCharCount[i] < 0) return null;
-   }
-   
-   LetterInventory newLetterInventory = new LetterInventory("");
-   // set the newLetterInventory size attribute 
-   newLetterInventory.size = newSize;
-   // set the newLetterInventory charCount attribute
-   newLetterInventory.charCount = newCharCount;
-   return newLetterInventory;
-  
+      if (newCharCount[i] < 0)
+        return null;
+    }
+
+    LetterInventory newLetterInventory = new LetterInventory("");
+    // set the newLetterInventory size attribute
+    newLetterInventory.size = newSize;
+    // set the newLetterInventory charCount attribute
+    newLetterInventory.charCount = newCharCount;
+    return newLetterInventory;
+
   }
-  
+
 }
--- hw1/LetterInventory/src/LetterInventoryClient.java
+++ hw1/LetterInventory/src/LetterInventoryClient.java
@@ -6,18 +6,18 @@ public class LetterInventoryClient {
           System.out.println("size = " + li.size());
           System.out.println("isEmpty = " + li.isEmpty());
           System.out.println(li.get('a'));
-          
-          li.set('a',2);
+
+          li.set('a', 2);
           System.out.println("li = " + li);
-          li.set('c',3);
+          li.set('c', 3);
           System.out.println("li = " + li);
-          li.set('b',0);
+          li.set('b', 0);
           System.out.println("li = " + li);
           System.out.println("size = " + li.size());
           LetterInventory li2 = new LetterInventory("+++bz123");
           LetterInventory li3 = li.add(li2);
           System.out.println("li3 = " + li3);
-          
+
      }
 
 }
