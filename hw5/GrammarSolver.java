51ee14c16f372330afe2ab797d6b78878afc0b16
Lim Chin Yan  (NCS) <chin.yan.lim@ncs.com.sg>
2023-04-08 20:14:22 +0800

    commiting hw5

--- hw5/GrammarSolver.java
+++ hw5/GrammarSolver.java
@@ -14,8 +14,37 @@ public class GrammarSolver {
       
       // instantiate theGrammarMap
       theGrammarMap = new TreeMap<>();
+      // loop through list and add the grammar into the map
+      for (String s : grammar) {
+      
+         String[] parts = s.split("::=");
+         if (theGrammarMap.containsKey(parts[0])) {
+            throw new IllegalArgumentException();
+         }
+         theGrammarMap.put(parts[0],parts[1]);
+      }
    
    }
+   
+   public boolean grammarContains(String symbol) {
+   
+      return theGrammarMap.containsKey(symbol);
+   }
+   
+   // pre: throw an IllegalArgumentException if the grammar does not
+   //      contain the given nonterminal symbol or if the number
+   //      of times is less than 0. 
+   public String[] generate(String symbol, int times) {
+   
+      if (!grammarContains(symbol) ||  times < 0) {
+         throw new IllegalArgumentException();
+      }
+      // replace the return statement below 
+      return new String[4];
+   }
 
-
+   public String getSymbols() {
+   
+      return theGrammarMap.keySet().toString();
+   }
 }
